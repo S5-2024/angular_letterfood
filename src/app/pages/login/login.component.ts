b28069b2c001ab0f56ad5656cdc153dee46baf4e
gabriellesote <gabrielle.sote@gmail.com>
2024-10-29 20:55:08 -0300

    estilização do background da  pagina de login e registro aplicado, junto com o script css

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit {
+  private curX = 0;
+  private curY = 0;
+  private tgX = 0;
+  private tgY = 0;
 
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    const interBubble = this.el.nativeElement.querySelector('.interactive');
+
+    const move = () => {
+      this.curX += (this.tgX - this.curX) / 20;
+      this.curY += (this.tgY - this.curY) / 20;
+      this.renderer.setStyle(
+        interBubble,
+        'transform',
+        `translate(${Math.round(this.curX)}px, ${Math.round(this.curY)}px)`
+      );
+      requestAnimationFrame(move);
+    };
+
+    window.addEventListener('mousemove', (event: MouseEvent) => {
+      this.tgX = event.clientX;
+      this.tgY = event.clientY;
+      move();
+    });
+  }
 }
