c922b2e84a74a267b8c2b756b77ee0c31ddb3026
Gabriel Evaristo <gabrielousadia12@gmail.com>
2024-11-01 12:55:30 -0300

    Refatornado codigo e transformando em modal

--- src/app/components/store-details/store-details.component.ts
+++ src/app/components/store-details/store-details.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import {MatTabsModule} from '@angular/material/tabs';
 @Component({
   selector: 'store-details',
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule],
   templateUrl: './store-details.component.html',
   styleUrl: './store-details.component.css'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { StoreDetailsComponent } from '../../components/store-details/store-details.component';
+import {Overlay, OverlayConfig, OverlayModule} from '@angular/cdk/overlay';
+import { CdkPortal,PortalModule } from '@angular/cdk/portal';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [StoreDetailsComponent],
+  imports: [StoreDetailsComponent, OverlayModule, PortalModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  @ViewChild(CdkPortal) portal !: CdkPortal
   
+  constructor(private overlay: Overlay){}
+
+
+  // Configurando e abrindo modal de detalhes da loja
+  openModal(){
+    const config = new OverlayConfig(
+      {
+        positionStrategy: this.overlay.position().global().centerHorizontally().centerVertically(),
+        hasBackdrop: true,
+        /* panelClass: "teste_class" */
+      }
+    );
+    const overlayRef = this.overlay.create(config)
+    //Quando clica no backdrop ele fecha o modal
+    overlayRef.backdropClick().subscribe( () => {overlayRef.detach()}); 
+    overlayRef.attach(this.portal)
+
+  }
+
 }
