b537c5395ae219c7a8864341e6468f0e35352391
gabriellesote <gabrielle.sote@gmail.com>
2024-11-06 20:55:31 -0300

    component perfil criado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { PerfilComponent} from './pages/perfil/perfil.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent, pathMatch: "full"},
-    {path: 'login',  component: LoginComponent}
+    {path: 'login',  component: LoginComponent},
+    {path: 'perfil',  component: PerfilComponent}
 ];
