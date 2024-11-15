a3028f144135b405e4980e65a63ef140437e530c
gabriellesote <gabrielle.sote@gmail.com>
2024-11-14 22:07:27 -0300

    iniciando do zero o perfil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import {LandingPageComponent} from './pages/landing-page/landing-page.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
     {path: '', component: LandingPageComponent, pathMatch:"full"},
     {path: 'homelander', component: HomeComponent, pathMatch: "full"},
-    {path: 'login',  component: LoginComponent}
+    {path: 'login',  component: LoginComponent},
+    {path: 'profile',  component: ProfileComponent}
 ];
