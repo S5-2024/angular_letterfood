89003f3f5d0076dcae7543778a7d916fee116ac8
gabriellesote <gabrielle.sote@gmail.com>
2024-11-18 14:31:55 -0300

    escondendo o navbar no about

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,62 +3,66 @@ import { RouterOutlet } from '@angular/router';
 import { MatIconRegistry, MatIconModule } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { environment } from '../environments/environment.development';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { NgIf } from '@angular/common';
 
-const MONEY_BAG = environment.icons['money-bag']
-const REVIEW_STAR = environment.icons['review-star']
-const DISTANCE_FOOT = environment.icons['distance-foot']
-const LOGOS_GOOGLE_ICON = environment.icons['logos--google-icon']
-const SOLAR_CAT_BOLD = environment.icons['solar--cat-bold']
-const IC_ROUND_SETTINGS = environment.icons['ic--round-settings']
-const HOME_2_FILL = environment.icons['home-2-fill']
-const BASELINE_SEARCH = environment.icons['baseline-search']
-const SOLAR_HEART_BOLD = environment.icons['solar-heart-bold']
-const TRESURE_IMAGE = environment.icons['tresure-image']
-const LOCATION_IMAGE = environment.icons['location-image']
-const ORDER_FOOD_IMAGE = environment.icons['order-food-image']
-const GITHUB_ICON = environment.icons['github-icon']
+const MONEY_BAG = environment.icons['money-bag'];
+const REVIEW_STAR = environment.icons['review-star'];
+const DISTANCE_FOOT = environment.icons['distance-foot'];
+const LOGOS_GOOGLE_ICON = environment.icons['logos--google-icon'];
+const SOLAR_CAT_BOLD = environment.icons['solar--cat-bold'];
+const IC_ROUND_SETTINGS = environment.icons['ic--round-settings'];
+const HOME_2_FILL = environment.icons['home-2-fill'];
+const BASELINE_SEARCH = environment.icons['baseline-search'];
+const SOLAR_HEART_BOLD = environment.icons['solar-heart-bold'];
+const TRESURE_IMAGE = environment.icons['tresure-image'];
+const LOCATION_IMAGE = environment.icons['location-image'];
+const ORDER_FOOD_IMAGE = environment.icons['order-food-image'];
+const GITHUB_ICON = environment.icons['github-icon'];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatIconModule,NgIf],
+  imports: [RouterOutlet, MatIconModule, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-
 export class AppComponent {
   title = 'letterfood';
-  
-  constructor(private router: Router){
-    const iconRegistry = inject(MatIconRegistry)
-    const sanitizer = inject(DomSanitizer)
-    
-    for(let icon in Object.entries(environment.icons)){
-      iconRegistry.addSvgIconLiteral(Object.entries(environment.icons)[icon][0], 
-            sanitizer.bypassSecurityTrustHtml(Object.entries(environment.icons)[icon][1]))
+  showHeader = true; // Controle da visibilidade do header
+  currentRoute: string = '';
 
+  constructor(private router: Router) {
+    const iconRegistry = inject(MatIconRegistry);
+    const sanitizer = inject(DomSanitizer);
+
+    for (let icon of Object.entries(environment.icons)) {
+      iconRegistry.addSvgIconLiteral(
+        icon[0],
+        sanitizer.bypassSecurityTrustHtml(icon[1])
+      );
+    }
   }
 
+  ngOnInit() {
+    // Monitorar mudanças na rota
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url; // Atualiza a rota atual
+        this.showHeader = !event.url.includes('/about'); // Oculta o header se for a rota /about
+      }
+    });
+  }
 
+  redirectToLogin() {
+    this.router.navigate(['/login']); // Define o caminho para a página de login
+  }
 
-  
-}
- 
+  redirectToHome() {
+    this.router.navigate(['']); // Define o caminho para a página inicial
+  }
 
-currentRoute: string = '';
-
-ngOnInit() {
-  this.router.events.subscribe(() => {
-    this.currentRoute = this.router.url; // Obtém a URL atual
-  });
-}
-
-redirectToLogin() {
-  this.router.navigate(['/login']); // Define o caminho para a página de login
-}
-redirectToProfile() {
-  this.router.navigate(['/profile']); // Define o caminho para a página de login
-}
+  redirectToProfile() {
+    this.router.navigate(['/profile']); // Define o caminho para o perfil
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import {LandingPageComponent} from './pages/landing-page/landing-page.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
     {path: '', component: LandingPageComponent, pathMatch:"full"},
     {path: 'homelander', component: HomeComponent, pathMatch: "full"},
     {path: 'login',  component: LoginComponent},
-    {path: 'profile',  component: ProfileComponent}
+    {path: 'profile',  component: ProfileComponent},
+    {path: 'about',  component: AboutComponent}
 ];
